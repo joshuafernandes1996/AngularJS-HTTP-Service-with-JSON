49aec8e74e050f4b526e281c25201dff7b8c5ad3
Joshua <jf0273@gmail.com>
2019-01-08 12:23:47 +0530

    AngularJS 1.6 http as a service using JSON

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 ///<reference path="angular.js" />
 var myApp = angular
             .module("myMod",[])
-            .controller("myController",function($scope,$http,$log){
+            .controller("myController",function($scope,myService,$log){
                 var vm = this;
-                $http.get("https://api.myjson.com/bins/14tv8e").then(function(response){
+                myService.getPost().then(function(response){
                     $scope.posts = response.data;
                     $log.info(response);
                 },function(reason){
                     $scope.error=reason.data;
                     $log.info(reason);
                 });
+            });
+
+            myApp.service("myService",function($http){
+                this.getPost = function(){
+                    return $http.get("https://api.myjson.com/bins/14tv8e");
+                }
             });
